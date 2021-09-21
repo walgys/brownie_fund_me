5f7690236771b7574ceb48fa21c67a9af83d8da8
Walter Gysin <walter@botmaker.com>
2021-09-21 06:39:18 -0300

    first commit

---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,29 @@
+from brownie import accounts, config, network, MockV3Aggregator
+from web3 import Web3
+
+DECIMALS = 8
+STARTING_PRICE = 200000000000
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+
+
+def get_account():
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
+        # Ganache account
+        return accounts[0]
+    else:
+        # env account
+        return accounts.add(config["wallets"]["from_key"])
+    # Brownie stored account
+    # account = accounts.load("walter1")
+
+
+def deploy_mocks():
+    print(f"The active network is {network.show_active()}")
+    print("Deploying Mocks...")
+    if len(MockV3Aggregator) <= 0:
+        MockV3Aggregator.deploy(DECIMALS, STARTING_PRICE, {"from": get_account()})
+    print("Mocks Deployed!")
